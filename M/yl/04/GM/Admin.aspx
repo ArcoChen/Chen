44930f95ddbf13f1f662c8e3dde76f3267ceb355
ArcoChen <chenq414@gmail.com>
2016-07-29 08:37:55 +0800

    wrote yl

---
+++ M/yl/04/GM/Admin.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="QRCode.Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>后台管理</title>
+    <link href="Styles/admin.css" rel="stylesheet" />
+    <script type="text/javascript" src="Script/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="Script/comm.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="setBrand">
+            <h1>品牌商管理</h1>
+            <p>已有品牌</p>
+            <div style="display: block; float: none; overflow: hidden;">
+                <ul class="brandList">
+                    <%
+                        foreach (var item in ListBrand)
+                        {
+                    %>
+                    <li><%= item.Brand %>  -  <%=item.Norms %></li>
+                    <%
+                        } %>
+                </ul>
+            </div>
+            <div style="display: block; float: none; overflow: hidden;">
+                选择品牌：<asp:DropDownList ID="ddlBrand" runat="server">
+                </asp:DropDownList>
+                输入规格<asp:TextBox runat="server" ID="txtBrand" />
+                <asp:Button Text="新增" runat="server" ID="btnAddBrand" OnClick="btnAddBrand_Click" />
+            </div>
+        </div>
+        <div id="setUrl">
+            选择品牌规格：<asp:DropDownList ID="ddlBrandNorms" runat="server">
+            </asp:DropDownList>
+            数量：
+            <asp:TextBox runat="server" ID="txtCount" />
+            <asp:Button Text="生成二维码链接" runat="server" ID="btnCreate" OnClick="btnCreate_Click" />
+            <div>
+                <table class="tbScan">
+                    <tr>
+                        <th>编码
+                        </th>
+                        <th>品牌
+                        </th>
+                        <th>访问地址
+                        </th>
+                        <th>扫码数量
+                        </th>
+                    </tr>
+                    <%
+                        foreach (var item in scanList)
+                        {
+                    %>
+                    <tr>
+                        <td>
+                            <%=item.Code %>
+                        </td>
+                        <td>
+                            <%= Common.Main.GetBrandStr(item.Brand) %>
+                        </td>
+                        <td>
+                            <%=item.URL %>
+                        </td>
+                        <td>
+                            <%=item.Count %>
+                        </td>
+                    </tr>
+                    <%
+                        } %>
+                </table>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ M/yl/04/GM/Script/red.js
@@ -0,0 +1,128 @@
+﻿var integral;
+var coin;
+var totalIntegral;
+$(function () {
+    //var openid = "o9tD6wmIvgIe6rRy8IUo-545ddM4";
+    var nickName = GetQuery("nickName");
+    totalIntegral = GetQuery("totalIntegral");
+    $("#nickName").text(nickName);
+    $("#totalIntegral").text(totalIntegral);
+    var openid = GetQuery("openid");
+    $(".bignav li a").each(function (i) {
+        $(this).click(function () {
+            switch (i) {
+                case 0:
+                    integral = 100;
+                    coin = 1;
+                    break;
+                case 1:
+                    integral = 190;
+                    coin = 2;
+                    break;
+                case 2:
+                    integral = 460;
+                    coin = 5;
+                    break;
+                case 3:
+                    integral = 900;
+                    coin = 10;
+                    break;
+            }
+            checkUserIntergral(openid, integral);
+        });
+    });
+    $(".redbutton").click(function () {
+        duductUserIntegral(openid, integral, coin);
+    });
+    $("#footer").click(function () {
+        location.href = "../WeChat/ExchangeDetails.html?openid=" + openid;
+    });
+});
+
+//显示灰色 jQuery 遮罩层 
+function showBg() {
+    var bh = $("#container").height();
+    var bw = $("#container").width();
+    $("#fullbg").css({
+        height: bh,
+        width: bw,
+        display: "block"
+    });
+    $("#dialog").show();
+    $(".redbutton").show();
+}
+//关闭灰色 jQuery 遮罩 
+function closeBg() {
+    $("#fullbg,#dialog").hide();
+}
+
+//检查用户是否足够积分
+function checkUserIntergral(openid, duduct) {
+    getAjax("/ProductListHandle.ashx", { "openid": openid, "duduct": duduct, "status": "0" }, function (data) {
+        if (data == "-1") {
+            //用户不存在
+            $("#dialog").css("background", 'url(../Images/nouser.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            showBg();
+            $(".redbutton").hide();
+        } else if (data == "0") {
+            //不够积分
+            $("#dialog").css("background", 'url(../Images/NO.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            showBg();
+            $(".redbutton").hide();
+        } else {
+            //足够积分
+            $("#dialog").css("background", 'url(../Images/' + duduct + 'coin.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            showBg();
+        }
+    });
+}
+//扣减积分
+function duductUserIntegral(openid, duduct, rmbcoin) {
+    getAjax("/ProductListHandle.ashx", { "openid": openid, "duduct": duduct, "rmbcoin": rmbcoin, "status": "1" }, function (data) {
+        if (data == "-2") {
+            //红包发送异常
+            $("#dialog").css("background", 'url(../Images/sysbad.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            $(".redbutton").hide();
+        } else if (data == "-1") {
+            //执行数据异常报错
+            $("#dialog").css("background", 'url(../Images/nouser.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            $(".redbutton").hide();
+        } else if (data == "0") {
+            //用户不存在
+            $("#dialog").css("background", 'url(../Images/nouser.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            $(".redbutton").hide();
+        } else if (data == "1") {
+            //用户积分不足
+            $("#dialog").css("background", 'url(../Images/NO.png) no-repeat');
+            $("#dialog").css("background-size", 'contain');
+            $(".redbutton").hide();
+        } else {
+            totalIntegral = parseInt(totalIntegral) - parseInt(duduct);
+            $("#totalIntegral").text(totalIntegral);
+            //兑换积分成功
+            openRedPackage(rmbcoin);
+        }
+    });
+}
+//打开红包
+function openRedPackage(rmbcoin) {
+    // 在带有red样式的div中添加shake-chunk样式
+    $('#dialog').addClass('rotate');
+    //// 点击按钮2000毫秒后执行以下操作
+    setTimeout(function () {
+        // 在带有red样式的div中删除shake-chunk样式
+        $('#dialog').removeClass('rotate');
+        // 将redbutton按钮隐藏
+        $('.redbutton').css("display", "none");
+        // 修改red 下背景图
+        $("#dialog").css("background", 'url(../Images/' + rmbcoin + 'rmb.png) no-repeat');
+        // 修改red-jg的css显示方式为块
+        $("#dialog").css("background-size", 'contain');
+    }, 2000);
+}
